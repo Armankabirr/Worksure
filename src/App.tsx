74fd803d931780e93ab7804d0c0845af689933bb
Armankabirr <armanutsho12@gmail.com>
2025-12-01 18:39:19 +0600

    scroll to top button added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Preloader from "./components/Preloader";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 const queryClient = new QueryClient();
 
@@ -35,6 +36,7 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <ScrollToTopButton />
       </TooltipProvider>
     </QueryClientProvider>
   );
