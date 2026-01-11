ebbeb5b56efed92fbee48670caad829ba3698dc1
Armankabirr <armanutsho12@gmail.com>
2026-01-11 23:17:12 +0600

    Add navigation to electrical-repairs page from Electrician view all button

--- src/pages/Electrician.tsx
+++ src/pages/Electrician.tsx
@@ -279,7 +279,7 @@ const Electrician = () => {
           </div>
 
           <div className="flex justify-center pt-8 animate-fade-up" style={{ animationDelay: "520ms" }}>
-            <Button className="rounded-full" variant="outline">
+            <Button className="rounded-full" variant="outline" onClick={() => navigate("/electrician/electrical-repairs")}>
               View All Services
             </Button>
           </div>
